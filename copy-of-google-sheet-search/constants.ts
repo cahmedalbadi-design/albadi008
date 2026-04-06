93e148ee4088cd04383f0490a52ffd779b1a410b
cahmedalbadi-design <c.ahmed.albadi@gmail.com>
2026-04-06 14:50:49 +0400

    Delete copy-of-google-sheet-search directory

--- copy-of-google-sheet-search/constants.ts
+++
@@ -1,4 +0,0 @@
-
-export const SHEET_ID = '1A4qu3FlUjr5xvhdHEfcHhyfNYDwGIzON9MAC-SoyM7Q';
-export const GID = '0';
-export const SHEET_CSV_URL = `https://docs.google.com/spreadsheets/d/${SHEET_ID}/export?format=csv&gid=${GID}`;
